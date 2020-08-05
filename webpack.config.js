8e89efd8eeffce7382f8c28060adb61a9ff0987f
Noah Rosenthal <noahrosenthal@comcast.net>
2020-08-05 17:03:25 -0600

    Add dist directory to public for gitignore, and excluded node_modules in webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 var path = require('path');
 var SRC_DIR = path.join(__dirname, '/client');
-var DIST_DIR = path.join(__dirname, '/public');
+var DIST_DIR = path.join(__dirname, '/public/dist');
 
 module.exports = {
   entry: `${SRC_DIR}/index.jsx`,
@@ -13,6 +13,7 @@ module.exports = {
       {
         test: /\.jsx?/,
         // include : SRC_DIR,
+        exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
           query: {
