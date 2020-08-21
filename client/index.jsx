d2174dcfbeb3432f19a9c0f6faed6447839fbaa0
Noah Rosenthal <noahrosenthal@comcast.net>
2020-08-21 10:25:37 -0600

    Add standardChrome.css to try to fix deployed visual changes

--- client/index.jsx
+++ client/index.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
+import './standardChrome.css';
 import './customStyle.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import App from './components/App.jsx';
 
+import App from './components/App.jsx';
 
 // Will have to somehow get product_id and product_name from proxy request to here
 ReactDom.render(<App questions={[]} product_id={5} product_name={'Heir Force Ones'}/>, document.getElementById('qaModule'));
