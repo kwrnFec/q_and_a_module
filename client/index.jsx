a983e0ecba613b4374a334e39abd9c9d67d48ae8
Noah Rosenthal <noahrosenthal@comcast.net>
2020-08-22 14:43:39 -0600

    Small visual tweaks

--- client/index.jsx
+++ client/index.jsx
@@ -7,4 +7,4 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './components/App.jsx';
 
 // Will have to somehow get product_id and product_name from proxy request to here
-ReactDom.render(<App questions={[]} product_id={3} product_name={'Heir Force Ones'}/>, document.getElementById('qaModule'));
+ReactDom.render(<App questions={[]} product_id={3} product_name={'YEasy 350'}/>, document.getElementById('qaModule'));
